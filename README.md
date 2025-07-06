00c9a788a38d85d98196ee397cb5170247fcdf37
deb9911 <rthruat92@gmail.com>
2025-07-06 18:40:05 +0530

    System setup initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # VHos
+## New Updates are as followed
+
+![Alt Text](/pkgs/multimedia/desktop_bg/VyomX_OS_1_Bluewhale_1.png)
+
+![Alt Text](/pkgs/multimedia/desktop_bg/VyomX_OS_1_wild_nature.png)
---
+++ system_mod.sh
@@ -0,0 +1,7 @@
+sudo apt install libpq-dev
+sudo apt install portaudio19-dev
+
+# After initializing of VENV this need to be installed. 
+pip install --no-cache-dir langchain-community -i https://pypi.org/simple
+pip install --no-cache-dir PyAudio
+
